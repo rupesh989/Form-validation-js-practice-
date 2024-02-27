0268578516e49883ef4f46786005d405da1045e0
rupesh989 <rupeshbavaskar24@gmail.com>
2024-02-27 17:20:18 +0530

    added name validatefunctions

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const submitBtn = document.getElementById("submitBtn");
 const nameError = document.getElementById("nameError");
+const emailError = document.getElementById('emailError');
+const passError = document.getElementById('passError');
 
 submitBtn.addEventListener("click", (e) => {
   e.preventDefault();
@@ -12,17 +14,35 @@ function validateName(){
   let name = document.getElementById('name').value;
 
   if(name.length == 0){
-      nameError.innerHTML = "Name is required";
+      nameError.innerHTML = "Name Is Required";
       nameError.previousElementSibling.classList.add('fa-xmark');
       return false;
   }
 
   if(!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)){
-      nameError.innerHTML = "Write full Name";
+      nameError.innerHTML = "Write Full Name";
       nameError.previousElementSibling.classList.add('fa-xmark');
       return false;
   }
   nameError.innerHTML = "";
   nameError.previousElementSibling.classList.add('fa-check');
+  return true;
+}
+function validateEmail(){
+  let email = document.getElementById('email').value;
+
+  if(email.length == 0){
+      emailError.innerHTML = "Email is required";
+      emailError.previousElementSibling.classList.add('fa-xmark');
+      return false;
+  }
+
+  if(!email.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)){
+      emailError.innerHTML = "Enter Valid Email";
+      emailError.previousElementSibling.classList.add('fa-xmark');
+      return false;
+  }
+  emailError.innerHTML = "";
+  emailError.previousElementSibling.classList.add('fa-check');
   return true;
 }
