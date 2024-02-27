8af2dd4eedb8be59a349e5b2fbea63d0c46594db
rupesh989 <rupeshbavaskar24@gmail.com>
2024-02-27 17:18:44 +0530

    added name validatefunctions

--- script.js
+++ script.js
@@ -8,25 +8,21 @@ submitBtn.addEventListener("click", (e) => {
     alert("Form submitted successfully")
   }
 });
+function validateName(){
+  let name = document.getElementById('name').value;
 
-function validateName() {
-  let name = document.getElementById("name").value;
-
-  if (name.length == 0) {
-    nameError.innerHTML = "Name is required ";
-    // nameError.previousElementSibling.classList.add("fa-xmark");
-
-    return false;
+  if(name.length == 0){
+      nameError.innerHTML = "Name is required";
+      nameError.previousElementSibling.classList.add('fa-xmark');
+      return false;
   }
+
   if(!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)){
-        nameError.innerHTML="Write Your Full Name";
-        // nameError.previousElementSibling.classList.add("fa-xmark");
-        return false
-        
-    }
-
-    nameError.innerHTML=""
-    // nameError.previousElementSibling.classList.add("fa-check");
-
-   return true;  
+      nameError.innerHTML = "Write full Name";
+      nameError.previousElementSibling.classList.add('fa-xmark');
+      return false;
+  }
+  nameError.innerHTML = "";
+  nameError.previousElementSibling.classList.add('fa-check');
+  return true;
 }
